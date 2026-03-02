e3f7be3da53ce39a41918764c0dc2e20d620f0b8
cf0jonathan <72806571+cf0jonathan@users.noreply.github.com>
2026-03-01 20:09:19 -0600

    Fixed some UI issues (#27)

--- app/src/main/java/com/team2/studentfitness/MainActivity.kt
+++ app/src/main/java/com/team2/studentfitness/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.team2.studentfitness.ui.screens.LoginScreen
 import com.team2.studentfitness.ui.theme.StudentFitnessTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,17 +23,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-// Updated to accept a 'modifier' to handle the Scaffold padding correctly
-@Composable
-fun LoginScreen(modifier: Modifier = Modifier) {
-}
-
-@Preview(showBackground = true)
-@Composable
-fun LoginPreview() {
-    StudentFitnessTheme {
-        LoginScreen()
     }
 }
