3719ff4a09a431244902017591f6443cdfd0a5d2
leoldfield <113254412+leoldfield@users.noreply.github.com>
2026-03-31 19:59:01 -0500

    Update MentalHealth.kt to align with other database tables and update MentalHealthDao to expand Dao call list (#84)

--- app/src/main/java/com/team2/studentfitness/database/MentalHealth.kt
+++ app/src/main/java/com/team2/studentfitness/database/MentalHealth.kt
@@ -1,12 +1,17 @@
 package com.team2.studentfitness.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "mental_health")
+@Entity
 data class MentalHealth(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "timestamp")
+    val timestamp: Long = System.currentTimeMillis(),
+    @ColumnInfo(name = "mood")
     val mood: String,
-    val reflection: String,
-    val timestamp: Long = System.currentTimeMillis()
+    @ColumnInfo(name = "reflection")
+    val reflection: String
 )
